70de4cd72d82995cebcdc3b460a8fddb2f4ca86a
KEN <kienle.itjob@gmail.com>
2021-09-02 15:43:46 +0700

    Ver2.0
    Application Development- fix bugs

---
+++ ApplicationDevelopment/Controllers/BaseController.cs
@@ -0,0 +1,9 @@
+﻿using System.Web.Mvc;
+
+namespace ApplicationDevelopment.Controllers
+{
+    [Authorize]
+    public class BaseController : Controller
+    {
+    }
+}
---
+++ ApplicationDevelopment/Entities/DataSeeding.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ApplicationDevelopment.Entities
+{
+    public class DataSeeding : DropCreateDatabaseAlways<TrainingProgramManagerDbContext>
+    {
+        protected override void Seed(TrainingProgramManagerDbContext context)
+        {
+            //IList<AspNetUser> defaultAspNetUser = new List<AspNetUser>();
+
+            //defaultAspNetUser.Add(new AspNetUser() { Fullname = "Đạt", Age=19});
+            //defaultAspNetUser.Add(new AspNetUser() { Fullname = "User 2"});
+            //defaultAspNetUser.Add(new AspNetUser() { Fullname = "User 3"});
+
+            //context.AspNetUsers.AddRange(defaultAspNetUser);
+
+            //base.Seed(context);
+            var users = new List<AspNetUser>
+            {
+                new AspNetUser{Fullname="Nguyễn Hoàng Duy", Email="abc", Contact="088888"},
+                new AspNetUser{Fullname="Lê Trung Kiên", Email="abc", Contact="099999"},
+            };
+            users.ForEach(s => context.AspNetUsers.Add(s));
+            context.SaveChanges();
+        }
+    }
+}
